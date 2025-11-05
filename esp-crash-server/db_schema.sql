f74baf9f1753b0d8395dc19d7b264ec703445d2f
Jimmy Wennlund <jimmy@wennlund.nu>
2025-11-05 10:54:25 +0100

    Fix search now

--- esp-crash-server/db_schema.sql
+++ esp-crash-server/db_schema.sql
@@ -5,10 +5,13 @@ CREATE TABLE elf_file (elf_file_id SERIAL PRIMARY KEY, "date" TIMESTAMP, project
 CREATE TABLE project_auth (project_auth_id SERIAL PRIMARY KEY, "date" TIMESTAMP, project_name TEXT, github TEXT);
 CREATE INDEX textsearch_idx ON crash USING GIN (textsearch);
 
+
 CREATE OR REPLACE FUNCTION update_textsearch() RETURNS TRIGGER AS $$
 DECLARE
     ext_device_id TEXT;
     device_alias TEXT;
+    elf_file_id INTEGER;
+    project_ver TEXT;
     project_alias TEXT;
 BEGIN
     -- Fetch the ext_device_id and alias from the device table
@@ -17,7 +20,7 @@ BEGIN
     WHERE d.device_id = NEW.device_id;
 
     -- Fetch the project_alias from the elf_file table
-    SELECT e.project_alias INTO project_alias
+    SELECT e.elf_file_id, e.project_alias, e.project_ver INTO elf_file_id, project_alias, project_ver
     FROM elf_file e
     WHERE e.project_name = NEW.project_name AND e.project_ver = NEW.project_ver;
 
@@ -31,7 +34,9 @@ BEGIN
         coalesce(NEW.project_name, '') || ' ' ||
         coalesce(NEW.project_ver, '') || ' ' ||
         coalesce(NEW.dump, '') || ' ' ||
-        coalesce(project_alias, ''));
+        coalesce(elf_file_id::TEXT, '') || ' ' ||
+        coalesce(project_alias, '') || ' ' ||
+        coalesce(project_ver, ''));
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -43,13 +48,16 @@ FOR EACH ROW EXECUTE FUNCTION update_textsearch();
 
 CREATE TABLE project_webhooks (webhook_id SERIAL PRIMARY KEY, project_name TEXT, webhook_url TEXT);
 CREATE TABLE project_slack_integrations (
-    slack_integration_id SERIAL PRIMARY KEY, 
-    project_name TEXT, 
-    slack_team_id TEXT, 
+    slack_integration_id SERIAL PRIMARY KEY,
+    project_name TEXT,
+    slack_team_id TEXT,
     slack_team_name TEXT,
-    slack_channel_id TEXT, 
+    slack_channel_id TEXT,
     slack_channel_name TEXT,
     slack_access_token TEXT,
     created_date TIMESTAMP DEFAULT NOW(),
     github_user TEXT
 );
+
+CREATE INDEX idx_elf_file_project ON elf_file (project_name, project_ver);
+CREATE INDEX idx_elf_file_project_name ON elf_file (project_name);
